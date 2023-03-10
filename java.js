e01feb2c0b18707f36edfcb18efe9a4f1c359929
username9896 <103965166+username9896@users.noreply.github.com>
2023-03-10 16:50:23 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,40 @@
+function myfunc() {
+  var input = document.getElementById("green").value;
+  document.getElementById('Bulb').src = 'https://i.postimg.cc/9z5yxyFx/green-bulb-removebg-preview.png';
+}
+
+function myfunc1() {
+  var input = document.getElementById("blue").value;
+  document.getElementById('Bulb').src = 'https://i.postimg.cc/2yRK1RLS/image-removebg-preview.png';
+}
+
+function myfunc2() {
+  var input = document.getElementById("yellow").value;
+  document.getElementById('Bulb').src = 'https://i.postimg.cc/SRzW7gq2/Screenshot-2023-03-07-181049-removebg-preview.png';
+}
+
+function myfunc3() {
+  var input = document.getElementById("off").value;
+  document.getElementById('Bulb').src = 'https://i.postimg.cc/KjK1wL3c/bulb-off.png';
+}
+
+
+function myfunc4() {
+  var input = document.getElementById("Green").value;
+  document.getElementById('bulb').src = 'https://i.postimg.cc/9z5yxyFx/green-bulb-removebg-preview.png';
+}
+
+function myfunc5() {
+  var input = document.getElementById("Blue").value;
+  document.getElementById('bulb').src = 'https://i.postimg.cc/2yRK1RLS/image-removebg-preview.png';
+}
+
+function myfunc6() {
+  var input = document.getElementById("Yellow").value;
+  document.getElementById('bulb').src = 'https://i.postimg.cc/SRzW7gq2/Screenshot-2023-03-07-181049-removebg-preview.png';
+}
+
+function myfunc7() {
+  var input = document.getElementById("Off").value;
+  document.getElementById('bulb').src = 'https://i.postimg.cc/KjK1wL3c/bulb-off.png';
+}
